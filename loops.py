b41d09fa65b3aa8f936d272a9837f30a4daa75a5
Rahul <rahul.1rn17ec117@gmail.com>
2023-05-20 16:56:57 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+#This is loops 
+# you can easily apply loop on any iterable element
+
+name="Rahul"  #String is  iterable
+colors=['red','green','orange'] # List is iterable similarly set is iterable , sets is iterable ,dictionary is iterable
+for i in name:
+    print(i)
+
+# range function can run the loop finite amount of time
+# range has 3 arguments range(start,stop,step)
+for i in range(0,20,2):
+    print(i)
+    
---
+++ whileloop.py
@@ -0,0 +1,13 @@
+# while loop is preferred over for loop when you have a predefined condition
+# But when you have to run the loop based on finite iteration we can go with for loop
+
+while int(input('enter a number'))<34:
+    print('hey')
+
+else:
+    print('you entered number greater than 34')
+
+#using else with while loop
+
+# do while is not used in python
+
