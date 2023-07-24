a7867b72b6b80a98c4162bddf52bae9b4179d278
Rahul <rahul.1rn17ec117@gmail.com>
2023-07-24 19:15:14 +0530

    This was unexpected

--- OS.py
+++ OS.py
@@ -2,10 +2,10 @@
 import os
 #print(dir(os))
 #os.mkdir("./../data") 
-if (not os.path.exists("./../data")):  #checks if the directpry exists
-    os.mkdir("./../data")
-for i in range(100):
-    os.mkdir(f"./../data/day{i+1}") #makes the directory
+# if (not os.path.exists("./../data")):  #checks if the directpry exists
+#     os.mkdir("./../data")
+# for i in range(100):
+#     os.mkdir(f"./../data/day{i+1}") #makes the directory
 
 
 #Os is a very vast module and you can google a lot of  os modules are available
@@ -13,6 +13,18 @@ for i in range(100):
 
 
 folders=os.listdir("./../data") #lists all the folders and stores in folder varible
-for i in folders:
-    print(i)
-  
+
+#our new exercise is to explore os module commands
+
+######################## This session advance session on os ##################################3
+
+print(os.getcwd()) #D:\python
+print(os.listdir())
+
+#how to change your current working directory
+#os.chdir(r"") #this will chabhe the current working directory
+
+for root,directory,files in os.walk(os.getcwd()):
+    #print(root) #gives all the root directory
+    print(directory) #give all the directories 
+    print(files)
